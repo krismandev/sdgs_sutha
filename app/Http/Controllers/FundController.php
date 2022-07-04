d0e755ab889594ec32f09b93a84f8f38f38d47b5
Alfiizzi21 <alfi.izzi.jalhik@gmail.com>
2022-07-04 11:16:02 +0700

    mengisi FundController

--- app/Http/Controllers/FundController.php
+++ app/Http/Controllers/FundController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fund;
 
 class FundController extends Controller
 {
-    
+    public function detailFund($id)
+    {
+        $fund = Fund::find($id);
+        $dana_masuk = "Rp " . number_format($fund->dana_masuk,2,',','.');
+        $target = "Rp " . number_format($fund->target,2,',','.');
+        $deadline = date_diff(date_create(),date_create($fund->deadline." 23:59:59.000000"))->d;
+        $persentasefund = explode('.',$fund->dana_masuk/$fund->target*100,2)[0]."%";
+        return view('frontend.fund.detailfund',compact(['fund','persentasefund','dana_masuk','target','deadline','id']));
+    }
+    public function formFund($id)
+    {
+        return view('frontend.fund.formfund',compact(['id']));
+    }
 }
