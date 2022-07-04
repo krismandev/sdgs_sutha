5fa8b034aec9cfb775997bcbdb1f59ad67188f2c
Alfiizzi21 <alfi.izzi.jalhik@gmail.com>
2022-07-04 11:38:13 +0700

    membuat tabel donaturs di database

---
+++ database/migrations/2022_07_04_043649_create_donaturs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDonatursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donaturs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('fund_id');
+            $table->text('nama');
+            $table->text('email');
+            $table->integer('jumlah');
+            $table->text('order_id')->nullable();
+            $table->text('payment_type')->nullable();
+            $table->text('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donaturs');
+    }
+}
